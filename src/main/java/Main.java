c7a2c8f1bb0fd11f14948f27a1cc3983753a5f2a
JakubForys4 <jakubforys09@gmail.com>
2025-05-14 14:27:52 +0000

    6.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces 
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class WrongStudentName extends Exception { }
 class WrongAge extends Exception { }
@@ -52,7 +53,16 @@ class Main {
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
         System.out.println("0 - aby wyjść z programu");
-        return scan.nextInt();
+        while (true) {
+            try {
+                return scan.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Błędny wybór! Proszę podać liczbę.");
+                scan.nextLine();
+            }
+        }
+
+        
     }
 
     public static String ReadName() throws WrongStudentName {
