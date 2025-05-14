b0bde22d7df391ca877e82ac25f46608c37221cb
JakubForys4 <jakubforys09@gmail.com>
2025-05-14 14:06:24 +0000

    6.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,7 @@ import java.util.Scanner;
 
 class WrongStudentName extends Exception { }
 class WrongAge extends Exception { }
+class WrongDateOfBirth extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -39,6 +40,8 @@ class Main {
                 System.out.println("Błędne imię studenta!");
             } catch(WrongAge e) {
                 System.out.println("Niepoprawny wiek! Wiek musi być z zakresu 1-99.");
+            }catch(WrongDateOfBirth e) {
+                System.out.println("Niepoprawna data urodzenia! Format musi być DD-MM-YYYY.");
             }
         }
     }
@@ -62,7 +65,7 @@ class Main {
         return name;
     }
 
-    public static void exercise1() throws IOException, WrongStudentName, WrongAge {
+    public static void exercise1() throws IOException, WrongStudentName, WrongAge, WrongDateOfBirth {
         var name = ReadName();
         System.out.println("Podaj wiek: ");
         var age = scan.nextInt();
@@ -71,6 +74,7 @@ class Main {
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYYY");
         var date = scan.nextLine();
+        validateDateFormat(date);
         (new Service()).addStudent(new Student(name, age, date));
     }
 
@@ -91,6 +95,11 @@ class Main {
         else {
             System.out.println("Znaleziono: ");
             System.out.println(wanted.ToString());
+        }
+    }
+    public static void validateDateFormat(String date) throws WrongDateOfBirth {
+        if (!date.matches("\\d{2}-\\d{2}-\\d{4}")) {
+            throw new WrongDateOfBirth();
         }
     }
 }
